26704305e3e4b1e07a938de1a333ede68c9ca29b
Smart-Chou <Z18538383923@gmail.com>
2021-11-18 06:50:29 +0000

    Deploying to gh-pages from @ Smart-Chou/smart-chou.github.io@ba190fcaf9ea5c731538ad4d6481545dce3afa83 🚀

---
+++ assets/js/102.e56f783f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[102],{516:function(n,w,o){}}]);
---
+++ data/articleData.js
@@ -0,0 +1,50 @@
+export default [
+    {
+      name: '在Cloudflare部署FODI后端',
+      url: '/document/fodi-cloudflare.html'
+    },
+    {
+      name: '目录列表程序AList使用教程',
+      url: '/document/alist-v2.html'
+    },
+    {
+      name: '用TiddlyWiki作为个人知识管理系统',
+      url: '/document/tiddywiki.html'
+    },
+    {
+      name: 'OpenWrite给VuePress配置博客导流公众号插件',
+      url: '/document/openwrite-to-vuepress.html'
+    },
+    {
+      name: '做网站怎么赚钱',
+      url: '/document/make-money.html'
+    },
+    {
+      name: '驰为Hi8安装Debian Linux',
+      url: '/document/chuwei-hi8-an-zhuang-debian-linux.html'
+    },
+    {
+      name: '宝塔面板部署Bitwarden密码管理软件',
+      url: '/document/bitwarden.html'
+    },
+    {
+      name: '使用宝塔docker安装为知笔记私有部署',
+      url: '/document/wiz-docker.html'
+    },
+    {
+      name: 'Windows Office安装指南',
+      url: '/daily/windows-office.html'
+    },
+    {
+      name: '奈飞简介',
+      url: '/daily/netflix-introduction.html'
+    },
+    {
+      name: '斐讯K2路由器刷openwrt',
+      url: '/document/k2-openwrt.html'
+    },
+    {
+      name: 'FODI，一款OneDrive秒级列表程序',
+      url: '/document/fodi.html'
+    }
+  ]
