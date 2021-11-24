a8fb62030db6f6432f097a52780ede2c6935262e
MIFSH <Z18538383923@gmail.com>
2021-11-24 12:03:35 +0800

    Update config.js

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,7 +1,35 @@
 const { config } = require("vuepress-theme-hope");
 const navBarConfig = require("./config/navbar");
 const sideBarConfig = require("./config/sideBar");
-
+const locale = {
+  nick: '昵称',
+  nickError: '昵称不能少于3个字符',
+  mail: '邮箱',
+  mailError: '请填写正确的邮件地址',
+  link: '网址',
+  placeholder: '随便说点什么喵～o(=•ェ•=)m \n没有登录也是完全没问题的哦～',
+  sofa: '来发评论吧~',
+  submit: '提交',
+  reply: '回复',
+  cancelReply: '取消回复',
+  comment: '评论',
+  more: '加载更多...',
+  preview: '预览',
+  emoji: '表情',
+  uploadImage: '上传图片',
+  seconds: '秒前',
+  minutes: '分钟前',
+  hours: '小时前',
+  days: '天前',
+  now: '刚刚',
+  uploading: '正在上传',
+  login: '登录',
+  logout: '退出',
+  admin: '博主',
+  word: '字',
+  wordHint: '评论字数应在 $0 到 $1 字之间！\n当前字数：$2',
+  };
+  
 module.exports = config({
   title: "CodeNoobs",
   description: "编程菜鸟的自我修养",
@@ -102,7 +130,6 @@ module.exports = config({
     comment: {
       type: 'waline',
       serverURL:'https://waline.codenoob.top/',
-      uploadImage: 'true',
       path:'window.location.pathname',
       lang:'zh-CN',
       visitor:'true',
@@ -115,12 +142,9 @@ module.exports = config({
           'https://cdn.jsdelivr.net/gh/walinejs/emojis/bilibili',
             ],
       meta:['nick', 'mail', 'link'],
-      requiredMeta:['nick'],
-      avatar:'',
-      avatarCDN:['https://sdn.geekzu.org/avatar/'],
-      avatarForce:'false',
-      copyright:'false',
-      pageSize:'10',
+      requiredMeta:['nick','mail'],
+      pageSize:'8',
+      locale,
       },
 
     copyright: {
