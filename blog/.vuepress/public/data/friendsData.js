fbffe3b72066ff3f6e708614b425bfda699c07bf
MIFSH <Z18538383923@gmail.com>
2021-12-06 13:02:08 +0800

    up rdg

--- blog/.vuepress/public/data/friendsData.js
+++ blog/.vuepress/public/data/friendsData.js
@@ -1,9 +1,21 @@
 export default [
     {
+      "name": "Mr.Hope", 
+      "desc": "Where there is light, there is hope.", 
+      "link": "https://mrhope.site/", 
+      "avatar": "https://cdn.jsdelivr.net/gh/Mister-Hope/Mister-Hope.github.io@latest/src/.vuepress/public/logo.png"      
+    },
+    {
       "name": "CodeNoobs", // 站点名称
       "desc": "编程菜鸟的自我修养", // 站点描述
       "link": "https://codenoob.top/", // 站点地址
       "avatar": "https://cdn.jsdelivr.net/gh/Smart-Chou/webphoto@latest/image/AvatarMaker-y.png" // 站点图片链接     
+    },
+    {
+      "name": "维安雨轩 の Blog",
+      "link": "https://blog.ukenn.top/",
+      "avatar": "https://cdn.jsdelivr.net/gh/Ukenn2112/UkennWeb@2.0/index/666.jpg",
+      "desc": "寒蝉黎明之时，便是重生之日"
     },
     {
       "name": "不方思", 
@@ -11,4 +23,82 @@ export default [
       "link": "https://irr.ink", 
       "avatar": "https://irr.ink/logo.jpg"   
     }
+    /*{
+      "name": "約束の明日へ",
+      "link": "https://asuhe.jp",
+      "avatar": "https://asuhe.jp/wp-content/uploads/2020/12/logo.png",
+      "desc": "また君を思い出すよ"
+    },
+    {
+      "name": "苍穹の下",
+      "link": "https://www.blueskyxn.com/", 
+      "avatar": "https://ae01.alicdn.com/kf/U86be0ebc85004924a57b3f81c8091f51Y.jpg", 
+      "desc": "SKYのBlog：一个IT打工人&老二刺螈&老游戏佬的博客"
+    },
+    {
+      "name": "So4msのBlog",
+      "link": "https://so4ms.top/",
+      "avatar": "https://so4ms.top/wp-content/uploads/2020/11/1602468269720.jpeg",
+      "desc": "一个正在学pwn的二进制弟弟"
+    },
+    {
+      "name": "不明のNetWorkOS",
+      "link": "https://networkos.club/",
+      "avatar": "https://networkos.club/wp-content/uploads/2021/01/head.jpg",
+      "desc": "五年代码，三年BUG"
+    },
+    {
+      "name": "沐风の秘密基地",
+      "link": "https://mf.xgzwlkj.cn",
+      "avatar": "https://mf.xgzwlkj.cn/wp-content/uploads/2021/01/91-e1610031977173.jpg",
+      "desc": "青春とは、七つの自分に会えること。"
+    },
+    {
+      "name":"Tokameine",
+      "link":"http://tokameine.top/",
+      "avatar":"http://tokameine.top/wp-content/uploads/2021/02/241972_1iMJPN7x.png",
+      "desc":"大脑它不受控制地拒绝着思考！"
+    },
+    {
+      "name": "三葉の后花园",
+      "link": "https://www.lvatc.com",
+      "avatar": "https://wl.lvatc.com/2020/12/timg.jpg",
+      "desc": "一个基于二次元的知识分享站"
+    },
+    {
+      "name": "漉鲸的Blog",
+      "link": "https://blog.lj0.top",
+      "avatar": "https://blog.lj0.top/favicon.ico",
+      "desc": "个人向，记录生活的博客。"
+    },
+    {
+      "name": "老兄de博客",
+      "link": "https://www.moec.top/",
+      "avatar": "https://www.moec.top/wp-content/uploads/2021/01/photo_2020-04-05_08-52-40.jpg",
+      "desc": "只要有光，目标再远，人们都能继续前行。带着希望，向着美好的未来飞翔"
+    },
+    {
+      "name": "抓住这个IDEA",
+      "link": "https://www.oyyandwjw.cn/",
+      "avatar": "https://gitee.com/Chien_W/pitcture/raw/master/login.jpg",
+      "desc": "鱼和熊掌不可兼得，但单身和穷可以！"
+    },
+    {
+      "name": "xiadengmaのblog",
+      "link": "https://blog.xiadengma.com",
+      "avatar": "https://cdn.jsdelivr.net/gh/XIADENGMA/IMGBED@main/avater/avatar.webp",
+      "desc": "隔在一层层纱后的你，在我心中"
+    },
+    {
+      "name": "WangScaler",
+      "desc": "记录开发爬坑的过程.",
+      "link": "http://www.scalerwang.com/blog/",
+      "avatar": "http://www.scalerwang.com/blog/logo.png"
+    },
+    {
+      "name": "w晚风阅览馆",
+      "link": "http://bk.xiat123.cn",
+      "avatar": "http://bk.xiat123.cn/head.jpg",
+      "desc": "流年以笑掷,未来诚可期!"
+    }*/
   ]
