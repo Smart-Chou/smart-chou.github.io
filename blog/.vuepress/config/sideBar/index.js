43c2c4913fb4029ee04968900df8a4b90c469d38
MIFSH <Z18538383923@gmail.com>
2022-03-23 23:02:12 +0800

    delete vaultwarden wiki

--- blog/.vuepress/config/sideBar/index.js
+++ blog/.vuepress/config/sideBar/index.js
@@ -2,8 +2,6 @@ const { sidebarConfig } = require("vuepress-theme-hope");
 
 module.exports = 
   sidebarConfig({
-    "/projects/vaultwarden/": require("./projects/vaultwarden"),
-
     "/tools/creat/": require("./tools/creat"),
     "/tools/": require("./tools"),
     
